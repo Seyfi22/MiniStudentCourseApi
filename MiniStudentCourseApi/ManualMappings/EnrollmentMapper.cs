674dc2aa10773af16c816a121055d149f8dddaff
Seyfi ISayev <isayevseyfi3277@gmail.com>
2025-10-29 08:22:33 +0400

    Auto Mapping & Manual Mapping setups added

--- MiniStudentCourseApi/ManualMappings/EnrollmentMapper.cs
+++ MiniStudentCourseApi/ManualMappings/EnrollmentMapper.cs
@@ -1,20 +1,46 @@
-﻿//using MiniStudentCourseApi.Model.DTOs;
-//using MiniStudentCourseApi.Model.Entities;
+﻿using MiniStudentCourseApi.Model.DTOs;
+using MiniStudentCourseApi.Model.Entities;
 
-//namespace MiniStudentCourseApi.ManualMappings
-//{
-//    public static class EnrollmentMapper
-//    {
-//        public static Enrollment MapEnrollmentDtoToEnrollment(EnrollmentDto dto)
-//        {
-//            return new Enrollment
-//            {
-//                Id = dto.Id,
-//                StudentId = dto.StudentId,
-//                CourseId = dto.CourseId,
-//                EnrollmentDate = dto.EnrollmentDate,
-//                Student.
-//            };
-//        }
-//    }
-//}
+namespace MiniStudentCourseApi.ManualMappings
+{
+    public static class EnrollmentMapper
+    {
+        public static Enrollment MapEnrollmentDtoToEnrollment(EnrollmentDto dto)
+        {
+            if(dto == null)
+            {
+                return null;
+            }
+
+
+            var enrollment = new Enrollment
+            {
+                Id = dto.Id,
+                StudentId = dto.StudentId,
+                CourseId = dto.CourseId,
+                EnrollmentDate = dto.EnrollmentDate
+            };
+
+            if(!string.IsNullOrEmpty(dto.Student))
+            {
+                var nameParts = dto.Student.Split(' ');
+
+                enrollment.Student = new Student
+                {
+                    FirstName = nameParts[0],
+                    LastName = nameParts.Length > 1 ? nameParts[1] : ""
+                };
+            }
+
+            if (!string.IsNullOrEmpty(dto.Course))
+            {
+                enrollment.Course = new Course
+                {
+                    Name = dto.Course
+                };
+            }
+
+            return enrollment;            
+        }
+    }
+}
